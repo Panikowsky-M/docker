c4d20721865f16b8208f3d73bfc1f8661f9a412c
Panikowsky-M <panikowsky.m@gmail.com>
2020-11-11 00:37:27 +0300

    Получение аватара вспомогательным контейнером

--- genavatar/src/avatargen.py
+++ genavatar/src/avatargen.py
@@ -1,8 +1,29 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
+
 app = Flask(__name__)
+DEFAULTNAME = 'Dmitry'
 
 @app.route('/')
-def hello():
-	return 'Hello from dockered Flask!\n'
+def indexHTML():
+	name = DEFAULTNAME
+	
+	head = '<html><head><meta charset=utf-8><title>Генератор аватаров</title></head><body>'
+	body = '''<form method=POST>
+		  Hello <input type=text name=name value={}>
+		  <input type=submit value=submit>
+		 </form>
+		 <p>Ваш аватар:</p>
+		<img src="/monster/monster.png"/>
+		'''.format(name)
+	tail = '</body></html>'
+	return head + body + tail
+
+@app.route('/monster/<name>')
+def get_icon(name):
+	r = requests.get('http://monsterpic:8080/monster/' + name + '?size=80')
+	pic = r.content
+	return Response( pic,mimetype='image/png' )
+
 if __name__ == '__main__':
 	app.run(debug=True,host='0.0.0.0')
