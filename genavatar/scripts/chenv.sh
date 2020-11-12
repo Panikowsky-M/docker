ad09e78546d4a160821e3912dcc79c3b01b84dd0
Panikowsky-M <panikowsky.m@gmail.com>
2020-11-12 19:57:59 +0300

    Разделение рабочих сред контейнера

---
+++ genavatar/scripts/chenv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+if [ "$ENV" == 'DEV' ]; then
+   echo "Запуск девы"
+   exec python "../src/avatargen.py"
+else
+  echo "Запуск прода" 
+  exec uwsgi --http 0.0.0.0:9090 --wsgi-file ../src/avatargen.py \
+       --callable app --stats 0.0.0.0:9191
+fi
